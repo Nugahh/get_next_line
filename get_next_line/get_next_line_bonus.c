abfda8dd993140d4f493110906809db15ac8b572
Nugah <fwong@e2r7p23.clusters.42paris.fr>
2022-06-19 14:34:58 +0000

    release

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fwong <fwong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 01:24:20 by fwong             #+#    #+#             */
-/*   Updated: 2022/06/19 14:25:23 by fwong            ###   ########.fr       */
+/*   Updated: 2022/06/19 14:25:28 by fwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
 	char		*buffer;
-	static char	*cache;
+	static char	*cache[1024];
 	char		*line;
 	ssize_t		red;
 
@@ -25,17 +25,17 @@ char	*get_next_line(int fd)
 	if (!buffer)
 		return (NULL);
 	red = 1;
-	while (red != 0 && !ft_is_newline(cache))
+	while (red != 0 && !ft_is_newline(cache[fd]))
 	{
 		red = read(fd, buffer, BUFFER_SIZE);
-		if (red == 0 && cache == NULL)
+		if (red == 0 && cache[fd] == NULL)
 			return (free(buffer), NULL);
 		if (red == -1)
-			return (free(cache), free(buffer), NULL);
+			return (free(cache[fd]), free(buffer), NULL);
 		buffer[red] = '\0';
-		cache = ft_strjoin(cache, buffer);
+		cache[fd] = ft_strjoin(cache[fd], buffer);
 	}
-	line = ft_get_line(cache);
-	cache = ft_get_endline(cache);
+	line = ft_get_line(cache[fd]);
+	cache[fd] = ft_get_endline(cache[fd]);
 	return (free(buffer), line);
 }
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwong <fwong@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 01:44:57 by fwong             #+#    #+#             */
+/*   Updated: 2022/06/19 14:16:27 by fwong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+
+size_t	ft_strlen(const char *s);
+int		ft_is_newline(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_get_line(char *str);
+char	*ft_get_endline(char *str);
+char	*get_next_line(int fd);
+
+#endif
